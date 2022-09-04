2aa6457b5e120ef4fe643d5993d26c4ab108b174
Pete Tran <peter_tran_cm@yahoo.com>
2022-09-04 18:56:11 -0400

    implemented gRPC server

--- modules/kafka_server/server.py
+++ modules/kafka_server/server.py
@@ -1,17 +1,39 @@
-from kafka import KafkaConsumer
-from kafka.errors import NoBrokersAvailable
-
+from kafka import KafkaProducer
+from concurrent import futures
 import location_pb2_grpc
+import location_pb2
+import json
 import grpc
 import os
 from google.protobuf.json_format import MessageToDict
 import logging
 
+TOPIC_NAME = 'location'
+KAFKA_SERVER = 'localhost:9092'
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+
+class LocationService(location_pb2_grpc.EventLocationServiceServicer):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def GetServerResponse(self, request, context):
+
+        request_value = {
+            'userId': int(request.userId),
+            'latitude': int(request.latitude),
+            'longitude': int(request.longitude)
+        }
+        msg = json.dumps(request_value, indent=2).encode('utf-8')
+        producer.send(TOPIC_NAME, msg)
+        return location_pb2.EventLocationMessage(**request_value)
+
 def serve():
-  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
-      RouteGuideServicer(), server)
-  server.add_insecure_port('[::]:50051')
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+  location_pb2_grpc.add_LocationServiceServicer_to_server(
+      LocationService(), server)
+  server.add_insecure_port('[::]:5005')
   server.start()
   server.wait_for_termination()
 
