d7f5c151f293cb59d2c73db2841c74925c4dd2b1
Pete Tran <peter_tran_cm@yahoo.com>
2022-09-13 16:07:48 -0400

    added getter to retrieve data from grpc

---
+++ modules/location_producer/getter.py
@@ -0,0 +1,16 @@
+import grpc
+import location_pb2
+import location_pb2_grpc
+
+"""
+Sample implementation of a getter that can be used to receive gRPC messages.
+"""
+
+print("Sending sample payload...")
+
+channel = grpc.insecure_channel(
+    "location-producer:5005")
+stub = location_pb2_grpc.LocationServiceStub(channel)
+
+response = stub.Get(location_pb2.LocationID(id=29))
+print(response)
