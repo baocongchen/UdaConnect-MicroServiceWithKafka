60736f73c65850582494a7e464cffd4eb89a5916
Pete Tran <peter_tran_cm@yahoo.com>
2022-09-14 13:10:52 -0400

    fixing error in grpc "Exception calling application: userId"

--- modules/location-producer/server.py
+++ modules/location-producer/server.py
@@ -6,28 +6,55 @@ import json
 import grpc
 import os
 from google.protobuf.json_format import MessageToDict
+from models import Location
 import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 TOPIC_NAME = 'location'
 KAFKA_URL = 'kafka:9092'
+DB_USERNAME = "ct_geoconnections"
+DB_PASSWORD = "d293aW1zb3NlY3VyZQ=="
+DB_HOST = "postgres-geoconnections"
+DB_PORT = 5432
+DB_NAME = "geoconnections"
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_URL)
 
 class LocationService(location_pb2_grpc.LocationServiceServicer):
 
     def __init__(self, *args, **kwargs):
-        pass
+        engine = create_engine(
+            f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+        SessionClass = sessionmaker(engine)
+        self.session = SessionClass()
+
+        self.TOPIC_NAME = 'locations'
+        KAFKA_SERVER = 'kafka:9092'
+        self.producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
     def Get(self, request, context):
 
-        request_value = {
-            'userId': int(request.userId),
-            'latitude': int(request.latitude),
-            'longitude': int(request.longitude)
-        }
-        msg = json.dumps(request_value, indent=2).encode('utf-8')
-        producer.send(TOPIC_NAME, msg)
-        return location_pb2.LocationMessageResponse(**request_value)
+        id = request.id
+        location = self.session.query(
+            Location).filter(Location.id == id).first()
+        print("Location: {}".format(location))
+        if location is None:
+            return location_pb2.LocationMessage(
+                id=id,
+                person_id=None,
+                longitude=None,
+                latitude=None,
+                creation_time=None
+            )
+        else:
+            return location_pb2.LocationMessage(**{
+                "id": location.id,
+                "person_id": location.person_id,
+                "longitude": location.longitude,
+                "latitude": location.latitude,
+                "creation_time": location.creation_time.isoformat(),
+            })
 
 def serve():
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
