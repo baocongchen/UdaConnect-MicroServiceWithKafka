5eeffa54a94df8800fd5a0dbce3631b2b9545610
Pete Tran <peter_tran_cm@yahoo.com>
2022-09-15 11:17:03 -0400

    allowed for passing argument

--- modules/location-consumer/getter.py
+++ modules/location-consumer/getter.py
@@ -1,16 +1,16 @@
 import grpc
 import location_pb2
 import location_pb2_grpc
-
+import sys
 """
 Sample implementation of a getter that can be used to receive gRPC messages.
 """
 
 print("Sending sample payload...")
-
+location_id = sys.argv[-1]
 channel = grpc.insecure_channel(
     "location-producer:5005")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
-response = stub.Get(location_pb2.LocationID(id=29))
+response = stub.Get(location_pb2.LocationID(id=location_id))
 print(response)
